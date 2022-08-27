9a546b4a0f734edb8717b7d6028f7fa1f6d7047a
M1RRoN <mds.myst@gmail.com>
2022-08-27 12:25:01 +0400

    step 1

---
+++ gendiff/gendiff.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+args = parser.parse_args()
+print(args.echo)
+
+
+'''
+gendiff -h
+usage: gendiff [-h] first_file second_file
+
+Compares two configuration files and shows a difference.
+
+positional arguments:
+  first_file
+  second_file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  '''

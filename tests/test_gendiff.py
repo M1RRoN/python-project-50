27d5908f73d2508b9863821acc3b116972f7f2f4
M1RRoN <mds.myst@gmail.com>
2022-09-20 17:05:50 +0400

    step 4

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,11 +2,17 @@ from gendiff.engine import generate_diff, run_gendiff
 import pytest
 
 
-def test_generate_diff():
-    answer = open(r'C:\Users\M1rr0n\python-project-50\tests\fixtures\result.txt')
-    assert run_gendiff(r'C:\Users\M1rr0n\python-project-50\tests\fixtures\file1.json', r'C:\Users\M1rr0n\python-project-50\tests\fixtures\file2.json') == print(answer)
+# def test_generate_diff():
+#     answer = open(r'C:\Users\M1rr0n\python-project-50\tests\fixtures\result.txt')
+#     assert run_gendiff(r'C:\Users\M1rr0n\python-project-50\tests\fixtures\file1.json',
+#                        r'C:\Users\M1rr0n\python-project-50\tests\fixtures\file2.json') == print(answer)
 
 
 def test_generate_diff_wrong_format():
     with pytest.raises(Exception):
         generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file3.wrong')
+
+
+def test_generate_diff():
+    answer = open('tests/fixtures/result.txt')
+    assert run_gendiff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == print(answer)
